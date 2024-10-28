a8ca452b7d4394f7bb62e105882d06212df985ad
Ali Zaiter <152106542+alizaiter777@users.noreply.github.com>
2024-10-29 01:04:25 +0200

    Update exc.py

--- exc.py
+++ exc.py
@@ -1,6 +1,8 @@
 x=int(input("Enter a number : "))
 sum_of_odd=0
+#if i odd then add it to sum nb 
 for i in range (x):
     if i%2!=0:
+        #add the odd numbers in sum of odd 
       sum_of_odd += i
 print(sum_of_odd)
